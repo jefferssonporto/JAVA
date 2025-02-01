53f92a916e4bbbcbf288eaf30392b732007cfaf7
Jefferson Porto <133730356+jefferssonporto@users.noreply.github.com>
2025-02-01 19:35:53 -0300

    código

---
+++ POO/Aplicações de POO/Projeto Livro/Livro/Publicacao.java
@@ -0,0 +1,16 @@
+            /* INTERFACE */
+            
+package projetolivro.projetolivro;
+
+/**
+ *
+ * @author Jeferson
+ */
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
+    
+}
